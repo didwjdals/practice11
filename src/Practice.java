c038c15e43d33bbf32e0aeaaaa068fe4c958dba7
didwjdals <jlpark0425@naver.com>
2024-01-30 17:42:17 +0900

    commit please

---
+++ src/Practice.java
@@ -0,0 +1,11 @@
+public class Practice {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 23;
+        int result = a+b;
+
+        System.out.println(result);;
+
+    }
+
+}
